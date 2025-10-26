09ff4c0c20840d87911880c9d6f95447d20650a6
ThanhDinh <58394144+snacklun@users.noreply.github.com>
2025-10-26 15:15:33 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 43.1 MB (79.23%)

---
+++ DSA LeetCode/0035-search-insert-position/solution.cs
@@ -0,0 +1,22 @@
+public class Solution {
+   public int SearchInsert(int[] nums, int target)
+    {
+        int left = 0;
+        int right = nums.Length - 1;
+
+        while (left <= right)
+        {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] < target)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+
+        // If not found, 'left' is the insertion position
+        return left;
+    }
+}
