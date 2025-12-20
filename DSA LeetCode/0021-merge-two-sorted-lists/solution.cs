cf152d5902ce27b29fe8d042097c8c148c85cd28
ThanhDinh <58394144+snacklun@users.noreply.github.com>
2025-12-20 13:39:02 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 43.6 MB (22.67%)

---
+++ DSA LeetCode/0021-merge-two-sorted-lists/solution.cs
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode MergeTwoLists(ListNode list1, ListNode list2)
+    {
+        // Dummy node to simplify merging
+        ListNode dummy = new ListNode(-1);
+        ListNode current = dummy;
+
+        while (list1 != null && list2 != null)
+        {
+            if (list1.val <= list2.val)
+            {
+                current.next = list1;
+                list1 = list1.next;
+            }
+            else
+            {
+                current.next = list2;
+                list2 = list2.next;
+            }
+
+            current = current.next;
+        }
+
+        // Attach remaining nodes
+        if (list1 != null)
+            current.next = list1;
+        else
+            current.next = list2;
+
+        return dummy.next;
+    }
+
+    
+}
