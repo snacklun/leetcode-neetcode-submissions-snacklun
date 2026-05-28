a6a3f48725e72262329a10088b0334e538f59400
ThanhDinh <58394144+snacklun@users.noreply.github.com>
2026-05-28 16:37:16 +0000

    Sync LeetCode submission Runtime - 7 ms (10.89%), Memory - 23.4 MB (80.56%)

--- DSA LeetCode/0344-reverse-string/solution.py
+++ DSA LeetCode/0344-reverse-string/solution.py
@@ -1,4 +1,9 @@
 class Solution:
     def reverseString(self, s: List[str]) -> None:
-        s.reverse()
+        reverse = []
+        for x in range (len(s)-1, -1, -1):
+            reverse.append(s[x]) 
+        s.clear()
+        s.extend(reverse)
+            
         
