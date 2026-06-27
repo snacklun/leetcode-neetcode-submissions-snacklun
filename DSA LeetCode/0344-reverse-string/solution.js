dda12f381aeef30ff7bd8a624e6f3c2f2e247e95
ThanhDinh <58394144+snacklun@users.noreply.github.com>
2026-06-27 17:52:10 +0000

    Sync LeetCode submission Runtime - 1 ms (61.88%), Memory - 60.9 MB (71.44%)

---
+++ DSA LeetCode/0344-reverse-string/solution.js
@@ -0,0 +1,16 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+    let i = 0;
+    let j = s.length -1;
+    while (i < j) {
+        const tmp = s[i];
+        s[i] = s[j];
+        s[j] = tmp;
+        
+        i+=1;
+        j-=1;
+    }
+};
